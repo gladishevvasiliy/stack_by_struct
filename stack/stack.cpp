40f31b9c382ca759e15adf033e92d9e10498e023
Vasiliy Gladishev <gladishevvasiliy@gmail.com>
2016-09-14 11:56:17 +0400

    add pop func

--- stack/stack.cpp
+++ stack/stack.cpp
@@ -4,8 +4,53 @@
 #include "stdafx.h"
 
 
-int main()
-{
-    return 0;
-}
+//define stack element
+struct elem {
+	int num;
+	elem *prev;
+};
+
+void push(int num, elem **top);
+int pop(elem**);
+void output(elem*);
+
+void push(int num, elem **top) {
+	elem *curr = new elem;
+	//(*curr).num = num ;
+	curr->num = num;
+	if (*top == NULL) {
+		curr->prev = NULL;
+	}
+	else {
+		curr->prev = *top;
+		*top = curr;
+	}
+}//end push
+
+int pop(elem **top) {
+	int tmp = (*top)->num;
+	if (tmp == NULL) {
+		return - 1;
+	}
+	else {
+		*top = (*top)->prev;
+		return tmp;
+	}
+}//end pop
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
